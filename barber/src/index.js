3a7ca442e41caa5f855290b75491728fa6b63c1d
KAA000UZB <166817003+KAA000UZB@users.noreply.github.com>
2024-04-17 16:46:06 +0500

    Gala - UI
    BY Fayyozbek

--- barber/src/index.js
+++ barber/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Gallery from './UI/abdulbosit.jsx'
+import Gala from './UI/Gala'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Gallery/>
+    <Gala></Gala>
     
   </React.StrictMode>
 );
