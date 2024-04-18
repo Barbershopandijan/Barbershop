8208fd9ee03b0a51ed3a6368129d812c48592e4c
Shoxnurmirzo <165065235+Shoxnurmirzo@users.noreply.github.com>
2024-04-18 14:39:12 +0500

    kesh_bulaishlamayapti
    shoxnurmirzo

--- barber/src/index.js
+++ barber/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Home from './Home.jsx'
-import Gallery from './UI/abdulbosit.jsx'
 import Gala from './UI/Gala';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Home/> */}
+    <Home/>
     <App />
     <Gala/>
     
